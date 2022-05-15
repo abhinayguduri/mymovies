f71024470265f8d28a355531bdff3bcc18717640
abhinayguduri <30427389+abhinayguduri@users.noreply.github.com>
2022-05-15 18:00:04 +0530

    SCROLL AND STYLING

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,28 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import { useParams } from 'react-router';
+import { useParams } from "react-router";
 
-import PageHeader from '../components/page-header/PageHeader';
-
-import { category as cate } from '../api/tmdbApi';
-import MovieGrid from '../components/movie-grid/MovieGrid';
+import PageHeader from "../components/page-header/PageHeader";
+import { useEffect } from "react";
+import { category as cate } from "../api/tmdbApi";
+import MovieGrid from "../components/movie-grid/MovieGrid";
 
 const Catalog = () => {
-
-    const { category } = useParams();
-
-    return (
-        <>
-            <PageHeader>
-                {category === cate.movie ? 'Movies' : 'TV Series'}
-            </PageHeader>
-            <div className="container">
-                <div className="section mb-3">
-                    <MovieGrid category={category}/>
-                </div>
-            </div>
-        </>
-    );
-}
+  const { category } = useParams();
+  useEffect(() => {
+    const start = () => {
+      window.scrollTo(0, 0);
+    };
+    start();
+  }, [category]);
+  return (
+    <>
+      <PageHeader>
+        {category === cate.movie ? "Movies" : "TV Series"}
+      </PageHeader>
+      <div className="container">
+        <div className="section mb-3">
+          <MovieGrid category={category} />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Catalog;
