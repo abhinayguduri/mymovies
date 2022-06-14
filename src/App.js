67d891d8033b918415fbd91c181077cf681c775a
abhinayguduri <30427389+abhinayguduri@users.noreply.github.com>
2022-06-14 13:50:37 +0530

    One Signal Install

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "swiper/swiper.min.css";
+import OneSignal from "react-onesignal";
+import { useEffect } from "react";
 import "./assets/boxicons-2.0.7/css/boxicons.min.css";
 import "./App.scss";
 
@@ -9,6 +11,12 @@ import Header from "./components/header/Header";
 import Routes from "./config/Routes";
 
 function App() {
+  useEffect(() => {
+    OneSignal.init({
+      appId: "a98791d4-94f5-4ec4-ae5f-caeee4800b0b",
+    });
+  }, []);
+
   return (
     <BrowserRouter>
       <Route
